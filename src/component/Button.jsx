a9c72ba1dd9f2037f665b74fa305de4cbbd9b176
Monika18092023 <mk.18092023@gmail.com>
2025-06-07 08:58:42 +0530

    hero section

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,7 +1,7 @@
 import { FaArrowRight } from "react-icons/fa";
 const Button = ({text, className}) => {
   return (
-    <div className={`flex items-center px-4 py-2 rounded-sm gap-2 ${className}`}>
+    <div className={`flex items-center px-3  font-bold py-2 rounded-sm gap-2 ${className}`}>
       <span>{text}</span> 
       <span><FaArrowRight /></span>
     </div>
